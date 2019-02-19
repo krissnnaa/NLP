33a0604a850819938d91fb4789ad1572c9c6c8e0
kpn3569 <kpn3569@rit.edu>
2019-02-19 16:14:48 -0500

    changes in code

--- ps1/ps11.py
+++ ps1/ps11.py
@@ -4,6 +4,8 @@
 Created on Tue Feb 19 13:46:34 2019
 
 @author: krishna
+
+Ref:https://www.nltk.org/book/
 """
 
 import nltk
@@ -13,7 +15,16 @@ import sys
 def listCompression(rawData):
     """
     Used to design and expore two list comphrension statements
+    compute modal verbs in the corpus
     """
+    wordList=rawData.split(' ')
+    #wordList=[w for w in wordData]
+    print(wordList[:3])
+    
+    modal=['can','may','must','might','will','would','should']
+    fdist = nltk.FreqDist(w.lower() for w in rawData)
+    for m in modal:
+        print(m + ':', fdist[m], end=' ')
     
     
 def regexManipulation(rawData):
@@ -30,5 +41,5 @@ if __name__== "__main__":
     url='https://www.gutenberg.org/files/219/219-0.txt' #sys.argv[1]
     response= request.urlopen(url)
     raw = response.read().decode('utf8')
-    #listCompression(raw)
+    listCompression(raw)
     #regexManipulation(raw)
