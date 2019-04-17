12ba7bcf92da909818a0d5fdf0f25b2d6600330e
krissnnaa <krissnnaa1988@gmail.com>
2019-04-17 14:37:29 -0400

    Update termprojectMainFile.py

--- termprojectMainFile.py
+++ termprojectMainFile.py
@@ -8,29 +8,32 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
-
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.utils.multiclass import unique_labels
 
 def creatingFeatures(testFile):
     if testFile is not None:
-        sourcePath = glob2.glob('/home/krishna/Downloads/termProject/test_data/**/source-tweet/*.json')
-        sourcePath = list(set(sourcePath))
-        with open('testFilePath.txt','w') as fd:
-            for item in sourcePath:
-                fd.write('%s\n'%item)
+        # sourcePath = glob2.glob('/home/krishna/Downloads/termProject/test_data/**/source-tweet/*.json')
+        # sourcePath = list(set(sourcePath))
+        # with open('testFilePath.txt','w') as fd:
+        #     for item in sourcePath:
+        #         fd.write('%s\n'%item)
         with open('testFilePath.txt', 'r') as fd:
             sourcePath = [l[:-1] for l in fd.readlines()]
     else:
-        sourcePath = glob2.glob('/home/krishna/Downloads/termProject/dataset/rumoureval-data/**/**/source-tweet/*.json')
-        sourcePath = list(set(sourcePath))
-        with open('trainFilePath.txt', 'w') as fd:
-            for item in sourcePath:
-                fd.write('%s\n' %item)
+        # sourcePath = glob2.glob('/home/krishna/Downloads/termProject/dataset/rumoureval-data/**/**/source-tweet/*.json')
+        # sourcePath = list(set(sourcePath))
+        # with open('trainFilePath.txt', 'w') as fd:
+        #     for item in sourcePath:
+        #         fd.write('%s\n' %item)
         with open('trainFilePath.txt', 'r') as fd:
             sourcePath = [l[:-1] for l in fd.readlines()]
 
     featureList = []
     sourceTweet = {}
-    
+
     for file in sourcePath:
         innerFeature = []
         innerSource = {}
@@ -44,24 +47,24 @@ def creatingFeatures(testFile):
             hashtag = 0
             url = 0
             for words in tokenWords:
-                if words[0].isupper():
+                if words[0].isupper(): # percentage of words that are upper
                     upperCount = upperCount + 1
-                if words == '#':
+                if words == '#': # if tweet has hastag
                     hashtag = 1
-                if words.startswith('http'):
+                if words.startswith('http'): # if tweet is referring to some url
                     url = 1
             innerFeature.append(hashtag)
             innerFeature.append(url)
             perUpper = upperCount / tokenCount
             innerFeature.append(perUpper)
-            retweet = jsonFile['retweet_count'] / 1000
+            retweet = jsonFile['retweet_count'] / 1000 # retweet count
             innerFeature.append(retweet)
-            if tokenWords[-1] == '?':
+            if tokenWords[-1] == '?':   # question mark at the end
                 questionMark = 1
             else:
                 questionMark = 0
             innerFeature.append(questionMark)
-            favorite = jsonFile['favorite_count'] / 1000
+            favorite = jsonFile['favorite_count'] / 1000 # favorite count
             innerFeature.append(favorite)
             innerSource[tweetID] = innerFeature
             sourceTweet.update(innerSource)
@@ -135,13 +138,58 @@ def ensembleClassifier(X,y,x_test,y_test):
     accuracyScore=clf.score(x_dev,y_dev)
     print('Ensemble Random Forest accuracy score for dev set=%0.2f'% accuracyScore)
 
-    clf.predict(x_test)
+    y_pred=clf.predict(x_test)
     accuracyScore = clf.score(x_test, y_test)
     print('Ensemble Random Forest  accuracy score for test set=%0.2f' % accuracyScore)
+    confusion_matrix_plot(y_test,y_pred)
+
+def confusion_matrix_plot(y_true, y_pred,normalize=True,cmap=plt.cm.Blues):
+    """
+    source:
+    https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+    """
+    title = 'Confusion matrix'
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # labels that appear in the data
+    classes = np.array(['True','False','Unverified'])
+    classes=classes[unique_labels(y_true, y_pred)]
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print(title)
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+
+    # All ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
 
 if __name__=='__main__':
 
-    #Preprocss and create features
+    #Preprocess and creating features
     sourceTweet=creatingFeatures(None)
     testsourceTweet=creatingFeatures('testfile')
     with open('/home/krishna/Downloads/termProject/dataset/traindev/rumoureval-subtaskB-train.json','r') as infile:
@@ -219,4 +267,4 @@ if __name__=='__main__':
     LinearSVMClassifier(X_data, y_data, x_test, y_test)
     #Ensemble Random forest
     ensembleClassifier(X_data, y_data, x_test, y_test)
-
+    plt.show()
